008aeda8d75cc930344cf400dd3df6f308b711e4
Santi Lorenzo <santirlorenzo@gmail.com>
2023-04-18 19:33:55 -0500

    logout functionality added

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import actions from "../actions/actions";
 
 const Nav = () => {
+  const dispatch = useDispatch();
+  const logout = () => {
+    window.localStorage.removeItem("loggedBBUser");
+    dispatch(actions.setUser(false));
+  };
+
   return (
     <nav className="nav">
       <Link className="link" to="/">
@@ -12,6 +20,9 @@ const Nav = () => {
       </Link>
       <Link className="link" to="/income">
         Income
+      </Link>
+      <Link onClick={logout} className="link" to="/login">
+        Logout
       </Link>
     </nav>
   );
